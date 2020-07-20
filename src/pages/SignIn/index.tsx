0bcb2fd68cdc0a420af25e224c18e4532025a3f9
Filubil <denison.shenry@gmail.com>
2020-07-20 12:22:56 -0300

    primeiro commit para debug no ios

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, Image } from 'react-native';
+
+import logo from '../../assets/logo.png';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logo} />
+      <Text>Sign IN</Text>
+    </Container>
+  );
+};
+
+export default SignIn;
